eae151eb22ffb9b41dca4590cc2f36cca07ef357
MindaForAll <mindaforall@gmail.com>
2024-11-14 13:45:51 -0700

    Create AboutUs.jsx

---
+++ Frontend/Components/AboutUs.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function AboutUs() {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>Learn more about our mission to support addiction recovery.</p>
+            <p>Placeholder</p>
+        </div>
+    );
+}
+
+export default AboutUs;
