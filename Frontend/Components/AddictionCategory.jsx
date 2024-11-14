e343aa18e78a72c25930a23cfab8f61355b1d455
MindaForAll <mindaforall@gmail.com>
2024-11-14 13:46:27 -0700

    Create AddictionCategory.jsx

---
+++ Frontend/Components/AddictionCategory.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AddictionCategory() {
+    return (
+        <div>
+            <h1>Addiction Category</h1>
+            <p>Here we talk about addiction.</p>
+        </div>
+    );
+}
+
+export default AddictionCategory;
